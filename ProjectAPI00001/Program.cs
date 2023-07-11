231ba4b8304b7ac38c2992b0cf893659975a8b4d
Sarmad Javed <46562878+Sarmadjavediqbal@users.noreply.github.com>
2023-07-11 23:06:19 +0500

    Update 0.0.1-4

--- ProjectAPI00001/Program.cs
+++ ProjectAPI00001/Program.cs
@@ -24,11 +24,20 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty;
+    });
+
+app.UseHsts();
 
 // Shows UseCors with CorsPolicyBuilder.
 app.UseCors(builder =>
@@ -39,6 +48,8 @@ app.UseCors(builder =>
 });
 
 app.UseHttpsRedirection();
+
+app.UseRouting();
 
 app.UseAuthorization();
 
